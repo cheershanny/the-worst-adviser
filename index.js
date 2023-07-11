4398140ba4828c22d52e76cf33d4bb1f167bf39c
Hanny Tran <cheershanny@gmail.com>
2023-07-11 13:10:33 +0100

    add location

--- index.js
+++ index.js
@@ -31,6 +31,45 @@ const tasksArr = [
 'Painting'
 ]
 
+const locationsArr = [
+    'Night market',
+    'Antique market',   
+    'Street food market',   
+    'Chinatown',    
+    'Little Italy', 
+    'Red light district',   
+    'Amusement park',   
+    'Carnival', 
+    'Film festival',    
+    'Food festival',    
+    'Beer festival',    
+    'Wine festival',    
+    'Fashion show', 
+    'Comic convention', 
+    'Gaming convention',    
+    'Music concert',    
+    'Comedy club',  
+    'Cabaret show', 
+    'Ballet performance',   
+    'Contemporary dance performance',   
+    'Jazz club',    
+    'Karaoke bar',  
+    'Nightclub',    
+    'Rooftop bar',  
+    'Irish pub',    
+    'Sushi bar',    
+    'Wine bar', 
+    'Coffee shop',  
+    'Magical forest filled with towering trees and glowing flowers',    
+    'Secluded island with crystal clear waters and white sandy beaches',    
+    'Underwater city with colorful fish and coral reefs',   
+    'Floating city in the clouds with airships and flying creatures',   
+    'Medieval castle with drawbridges, turrets, and secret passages',   
+    'Futuristic city with towering skyscrapers and advanced technology',    
+    'Haunted mansion with creaky floorboards and spooky ghosts',    
+    'Space station orbiting distant planet',    
+]
+
 const benefitsArr = [
     'will make you happier',
     'will make you healthier',
@@ -44,13 +83,15 @@ const benefitsArr = [
     'might feel better than your ex-boyfriends'
 ]
 
+
 // end message: 
-// Do you know? <Doing something> <give you the benefit>
+// Do you know? <Doing something> in the <location>.lowercase <give you the benefit>
 
 const message = () => {
     let taskIndex = Math.floor(Math.random() * (tasksArr.length));
     let benefitIndex = Math.floor(Math.random() * (benefitsArr.length));
-    return `Do you know, ${tasksArr[taskIndex]} ${benefitsArr[benefitIndex]}.`;
+    let locationIndex = Math.floor(Math.random() * (locationsArr.length));
+    return `Do you know, ${tasksArr[taskIndex].toLowerCase()} in the ${locationsArr[locationIndex].toLowerCase()} ${benefitsArr[benefitIndex]}.`;
 }
 
 console.log(message());
